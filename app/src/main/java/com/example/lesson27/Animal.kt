1796aa80dcf36fdf25010a41fcd10e685ee90a53
glebov99R <kaspernenenepro@gmail.com>
2022-07-13 16:54:35 +0400

    AddCommentInEditActivity

--- app/src/main/java/com/example/lesson27/Animal.kt
+++ app/src/main/java/com/example/lesson27/Animal.kt
@@ -4,4 +4,8 @@ import java.io.Serializable
 import java.text.ParsePosition
 import java.util.*
 
+/**
+ * Класс Animal сюда приходит: / ID изображений(ImageId) / текст в поле tvTitle (title) / текст в поле edDesc(desc)
+ */
+
 data class Animal(val ImageId: Int, val title : String , val desc:String) : Serializable
--- app/src/main/java/com/example/lesson27/EditActivity.kt
+++ app/src/main/java/com/example/lesson27/EditActivity.kt
@@ -1,17 +1,22 @@
 package com.example.lesson27
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.lesson27.databinding.ActivityEditBinding
 
 class EditActivity : AppCompatActivity() {
-    lateinit var binding : ActivityEditBinding
+    lateinit var binding: ActivityEditBinding
+
+    /**
+     * Объявляем переменные индекса и ID изображений
+     */
     private var indexImage = 0
     private var ImageId = R.drawable.animal1
-    val u = 5
 
-
+    /**
+     * Заполняем список ImageIdList изображениями из папки drawable
+     */
     private val imageIdList = listOf(
         R.drawable.animal1,
         R.drawable.animal2,
@@ -23,27 +28,35 @@ class EditActivity : AppCompatActivity() {
         R.drawable.animal8,
     )
 
+    /**
+     * Функция onCreate запустится вместе с данным activity
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityEditBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        initButtons()
+        initButtons() // Инициализация функции
     }
-    private fun initButtons()= with(binding) {
 
-        bNext.setOnClickListener{
-            indexImage++
-            if (indexImage > imageIdList.size -1) indexImage = 0
-            ImageId = imageIdList[indexImage]
-            imageView.setImageResource(ImageId)
+    /**
+     * В функцию initButton помещаем два слушателя нажатий на кнопки / NEXT IMAGE / DONE /
+     */
+
+    private fun initButtons() = with(binding) { // with(binding) == binding.indexImage,..
+
+        bNext.setOnClickListener { // слушатель нажатий на кнопку NEXT IMAGE
+            indexImage++ // с каждым нажатием на кнопку NEXT IMAGE индекс увеличивается на 1
+            if (indexImage > imageIdList.size - 1) indexImage = 0 // Если индекс больше длины списка, то индекс обнулсяется
+            ImageId = imageIdList[indexImage] // Если индекс не превышает длины списка происходит переход на следующее изображение в списке
+            imageView.setImageResource(ImageId) // imageView визуализирует изображение
         }
-        bDone.setOnClickListener {
-            val result = Animal(ImageId, edTitle.text.toString(), edDesc.text.toString())
-            val editIntent = Intent().apply{
-                putExtra("animal", result)
+        bDone.setOnClickListener { // Слушатель нажатий кнопки DONE
+            val result = Animal(ImageId, edTitle.text.toString(), edDesc.text.toString()) // В result присваиваются параметры класса Animal в формате ID и текста
+            val editIntent = Intent().apply { // В переменную editIntent присваивается  объекст класса Intent
+                putExtra("animal", result) // Намерение отправить result на другое Activity которые примет его по слову "animal"
             }
-            setResult(RESULT_OK, editIntent)
-            finish()
+            setResult(RESULT_OK, editIntent) // Устанавливаем результат
+            finish() // закрывает данное Activity
         }
     }
 }
